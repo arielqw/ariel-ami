762d712fe6590a545b18b6a5bd1c5759a9c5c582
ami-bgu <orenamih@post.bgu.ac.il>
2013-12-14 17:25:16 +0200

    ass3a initial

---
+++ Assignment3/src/Ingredient.java
@@ -0,0 +1,10 @@
+
+public class Ingredient
+{
+
+	public Ingredient(String string, int i)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Assignment3/src/WarehouseImpl.java
@@ -0,0 +1,59 @@
+import java.util.List;
+
+
+public class WarehouseImpl implements Warehouse
+{
+
+
+	
+	@Override
+	public void takeResources(List<Ingredient> ingredients,
+			List<KitchenTool> kitchenTools)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void returnKitchenTools(List<KitchenTool> kitchenTools)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String ingredientsConsumedReport()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getIngredientsCount()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getKitchenToolCount()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRemainingIngredient(String ingredient)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRemainingKitchenTools(String kitchenTools)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
