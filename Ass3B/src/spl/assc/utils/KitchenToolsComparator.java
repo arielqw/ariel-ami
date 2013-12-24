004c7aee79ffb93b3f46e1cdad3303eb8f8dce3a
ami-bgu <orenamih@post.bgu.ac.il>
2013-12-24 04:09:09 +0200

    a

--- Ass3B/src/spl/assc/utils/KitchenToolsComparator.java
+++ Ass3B/src/spl/assc/utils/KitchenToolsComparator.java
@@ -8,8 +8,8 @@ public class KitchenToolsComparator implements Comparator<KitchenTool> {
 
 	@Override
 	public int compare(KitchenTool o1, KitchenTool o2) {
-		return o1.hashCode() - o2.hashCode();
-		//return o1.getName().compareTo(o2.getName());
+		//return o1.hashCode() - o2.hashCode();
+		return o1.getName().compareTo(o2.getName());
 	}
 
 }
