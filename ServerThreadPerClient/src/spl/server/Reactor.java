8e051d2ec6173a0611e455388b81bb9f7ecec51c
arielbaruch <arielbaruch@10.0.0.2>
2014-01-13 15:30:38 +0200

    hn

---
+++ ServerThreadPerClient/src/spl/server/Reactor.java
@@ -0,0 +1,11 @@
+package spl.server;
+
+public class Reactor implements ServerConcurrencyModel {
+
+	@Override
+	public void apply(Runnable connectionHandler) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
