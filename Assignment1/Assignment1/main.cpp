3fba17231aba2a81447e83995e73ec960f2f3e3d
Ami <amioren10@gmail.com>
2015-03-30 11:55:36 +0300

    only sobel operator left todo

--- Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/main.cpp
@@ -3,25 +3,26 @@
 #include <windows.h>
 #include "GL\glut.h"
 
-GLuint texture[4];
+enum Filter { F_NONE, F_SOBEL, F_HALFTONE, F_FLOYD };
 
-GLubyte *pic1;
-GLint width1;
-GLint height1;
-GLubyte *newpic, *newpic2, *newpic3;
-GLubyte *tmp2;
+GLuint texture[4];
+GLubyte* newPics[4];
+
+GLubyte* originalPic;
+GLint original_width;
+GLint original_height;
 
 void writeArrayToFile(char questionNum, GLubyte* content, int size, BOOL isMonochrome)
 {
 	char filename[30];
-	strcpy(filename, "assignment\\imgX.txt");
+	strcpy_s(filename, "assignment\\imgX.txt");
 	filename[14] = questionNum;
-	FILE * pFile;
+	FILE* pFile;
 	CreateDirectory("assignment", NULL);
-	pFile = fopen(filename, "wb");
+	fopen_s(&pFile, filename, "wb");
 	if (isMonochrome)
 	{
-		for (size_t i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)
 		{
 			if (content[i] == 255)	fwrite("1", sizeof(char), 1, pFile);
 			else					fwrite("0", sizeof(char), 1, pFile);
@@ -32,11 +33,11 @@ void writeArrayToFile(char questionNum, GLubyte* content, int size, BOOL isMonoc
 	}
 	else
 	{
-		for (size_t i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)
 		{
 			int value = content[i] / 16;
 			char str[4];
-			_itoa(value, str, 10);
+			_itoa_s(value, str, 10);
 			fwrite(str, sizeof(char)*strlen(str), 1, pFile);
 
 			if (i < size - 1)	fwrite(",", sizeof(char), 1, pFile);
@@ -45,93 +46,120 @@ void writeArrayToFile(char questionNum, GLubyte* content, int size, BOOL isMonoc
 	fclose(pFile);
 }
 
-void halftone(GLubyte *tmp)
+void sobel(GLubyte* pic)
 {
-	memset(tmp, 255, height1*width1 * 4 * sizeof(GLubyte));
+	GLdouble sx[3][3] = {	{ -1, 0, 1 },
+							{ -2, 0, 2 }, 
+							{ -1, 0, 1 }	};
 
-	for (int i = 0; i<height1; i++)
+	GLdouble sx[3][3] = {	{ 1,   2,  1 },
+							{ 0,   0,  0 },
+							{ -1, -2, -1 }	};
+
+
+}
+
+void halftone(GLubyte* pic)
+{
+	memset(pic, 255, original_height*original_width * 4 * sizeof(GLubyte));
+
+	for (int i = 0; i<original_height; i++)
 	{
-		for (int j = 0; j<width1; j++)
+		for (int j = 0; j<original_width; j++)
 		{			
 			int k=j*2;
 			int l=i*2;
 
-			if(pic1[i+j*width1]<=255*1/5.0)
-			tmp[l+k*width1*2]=0;
+			if(originalPic[i+j*original_width]<=255*1/5.0)
+			pic[l+k*original_width*2]=0;
 
-			if(pic1[i+j*width1]<=255*2/5.0)
-			tmp[l+1+k*width1*2]=0;
+			if(originalPic[i+j*original_width]<=255*2/5.0)
+			pic[l+1+k*original_width*2]=0;
 
-			if(pic1[i+j*width1]<=255*3/5.0)
-			tmp[l+1+(k+1)*width1*2]=0;
-			if(pic1[i+j*width1]<=255*4/5.0)
-			tmp[l+(k+1)*width1*2]=0;
+			if(originalPic[i+j*original_width]<=255*3/5.0)
+			pic[l+1+(k+1)*original_width*2]=0;
+			if(originalPic[i+j*original_width]<=255*4/5.0)
+			pic[l+(k+1)*original_width*2]=0;
 
 		}
 	}
-
-	//gluScaleImage(GL_LUMINANCE,width1*3,height1*3,GL_UNSIGNED_BYTE,tmp,width1,height1,GL_UNSIGNED_BYTE,newpic);
-	/*
-	for(int i=0;i<width1*2;i++)
-	{
-	for(int j=0;j<height1;j++)
-	{
-	if(newpic[i+j*width1]>48)
-	newpic[i+j*width1]=255;
-	else newpic[i+j*width1]=0;
-	}
-	}*/
-
-	//gluScaleImage(GL_LUMINANCE,width1*2,height1*2,GL_UNSIGNED_BYTE,newpic1,width1,height1,GL_UNSIGNED_BYTE,newpic);
-	/*
-	for(int i=0;i<width1*2;i++)
-	{
-	for(int j=0;j<height1;j++)
-	{
-	if(newpic[i+j*width1]>64)
-	newpic[i+j*width1]=255;
-	else newpic[i+j*width1]=0;
-	}
-	}
-	*/
-
 }
 
-void details()
+void floyd(GLubyte* pic)
 {
-	GLfloat *tmp1;
+	int numOfColors = 16;
+
+	GLdouble* tmp = new GLdouble[original_width*original_height];
+
+	for (int i = 0; i < original_width*original_height; i++) {
+		tmp[i] = (GLdouble)originalPic[i];
+	}
+
+	GLdouble a = 7.0 / 16.0;
+	GLdouble b = 3.0 / 16.0;
+	GLdouble c = 5.0 / 16.0;
+	GLdouble d = 1.0 / 16.0;
+
+	for (int i = 0; i < original_height; i++) {
+		for (int j = 0; j < original_width; j++) {
+			tmp[i*original_width + j] -= (((int)tmp[i*original_width + j]) % (256/numOfColors));
+			
+			double e = originalPic[i*original_width + j] - tmp[i*original_width + j];
+
+			if (j+1 < original_width)				tmp[i*original_width + j + 1]		+= (a*e);
+			if (i + 1 < original_height)
+			{
+				if		(j - 1 < original_width)		tmp[(i + 1)*original_width + j - 1] += (b*e);
+				else if (j < original_width)			tmp[(i + 1)*original_width + j]		+= (c*e);
+				else if (j + 1 < original_width)		tmp[(i + 1)*original_width + j + 1] += (d*e);		
+			}		
+		}
+	}
+	
+	for (int i = 0; i < original_width; i++) {
+		for (int j = 0; j < original_height; j++) {
+			pic[i*original_width + j] = (GLubyte)tmp[i*original_width + j];
+		}
+	}
+
+	delete[] tmp;
+	
+}
+
+void details(GLubyte* pic)
+{
+	GLfloat *tmp1 = new GLfloat[original_height*original_width];
 	GLfloat log[9] = { 0.4, 0.8, 0.4, 0.8, -3.82, 0.8, 0.4, 0.8, 0.4 };
 	int sum;
-	tmp1 = new GLfloat[height1*width1];
 
-	for (int i = 0; i<width1; i++)
+	for (int i = 0; i<original_width; i++)
 	{
-		for (int j = 0; j<height1; j++)
+		for (int j = 0; j<original_height; j++)
 		{
 			sum = 0;
-			tmp1[i + j*width1] = 0;
+			tmp1[i + j*original_width] = 0;
 			for (int k = i - 1; k <= i + 1; k++)
 				for (int l = j - 1; l <= j + 1; l++)
-					if (l >= 0 && k >= 0 && l<height1 && k<width1)
+					if (l >= 0 && k >= 0 && l<original_height && k<original_width)
 					{
-						tmp1[i + j*width1] += pic1[k + l*width1] * log[i - k + 1 + 3 * (j - l + 1)];
+						tmp1[i + j*original_width] += originalPic[k + l*original_width] * log[i - k + 1 + 3 * (j - l + 1)];
 					}
 		}
 	}
-	memset(newpic2, 0, height1*width1*sizeof(GLubyte));
+	memset(pic, 0, original_height*original_width*sizeof(GLubyte));
 
 	printf("\n***********details*****************\n");
-	for (int i = 0; i<width1 * 6 + 500; i++)
+	for (int i = 0; i<original_width * 6 + 500; i++)
 		printf("%0.2f  ", tmp1[i]);
 
 	printf("\n****************************\n");
 
-	for (int i = 1; i<width1; i++)
+	for (int i = 1; i<original_width; i++)
 	{
-		for (int j = 0; j<height1; j++)
+		for (int j = 0; j<original_height; j++)
 		{
-			if (tmp1[i + j*width1] * tmp1[i - 1 + j*width1]<-650)
-				newpic2[i + j*width1] = 255;
+			if (tmp1[i + j*original_width] * tmp1[i - 1 + j*original_width]<-650)
+				pic[i + j*original_width] = 255;
 		}
 	}
 	/*
@@ -149,87 +177,53 @@ void details()
 	delete tmp1;
 }
 
-void dithering()
+void dithering(GLubyte* pic)
 {
 	float *tmp1;
 	int colorNum = 16;
 	int sum;
-	tmp1 = new float[height1*width1];
-	memset(tmp1, 0, sizeof(float)*height1*width1);
-	for (int i = 0; i<width1; i++)
+	tmp1 = new float[original_height*original_width];
+	memset(tmp1, 0, sizeof(float)*original_height*original_width);
+	for (int i = 0; i<original_width; i++)
 	{
-		for (int j = 0; j<height1; j++)
+		for (int j = 0; j<original_height; j++)
 		{
 			sum = 0;
-			tmp1[i + j*width1] += pic1[i + j*width1];
-			while (tmp1[i + j*width1] >= 256 / colorNum && sum<255)
+			tmp1[i + j*original_width] += originalPic[i + j*original_width];
+			while (tmp1[i + j*original_width] >= 256 / colorNum && sum<255)
 			{
 				sum += 256 / colorNum;
-				tmp1[i + j*width1] -= 256 / colorNum;
+				tmp1[i + j*original_width] -= 256 / colorNum;
 			}
 			if (sum<255)
-				newpic3[i + j*width1] = sum;
-			else newpic3[i + j*width1] = 240;
-			if (i + j*width1 + 1<width1*height1)
-				tmp1[i + j*width1 + 1] += tmp1[i + j*width1] * 7.0 / 16.0;
-			if (j<height1 - 1)
+				pic[i + j*original_width] = sum;
+			else pic[i + j*original_width] = 240;
+			if (i + j*original_width + 1<original_width*original_height)
+				tmp1[i + j*original_width + 1] += tmp1[i + j*original_width] * 7.0 / 16.0;
+			if (j<original_height - 1)
 			{
-				if (i<width1 - 1)
-					tmp1[i + (1 + j)*width1 + 1] += tmp1[i + j*width1] * 1 / 16.0;
-				tmp1[i + (1 + j)*width1] += tmp1[i + j*width1] * 5.0 / 16.0;
-				tmp1[i + (1 + j)*width1 - 1] += tmp1[i + j*width1] * 3.0 / 16.0;
+				if (i<original_width - 1)
+					tmp1[i + (1 + j)*original_width + 1] += tmp1[i + j*original_width] * 1 / 16.0;
+				tmp1[i + (1 + j)*original_width] += tmp1[i + j*original_width] * 5.0 / 16.0;
+				tmp1[i + (1 + j)*original_width - 1] += tmp1[i + j*original_width] * 3.0 / 16.0;
 			}
 		}
 	}
 
 	printf("\n****************************\n");
 	for (int i = 0; i<264; i++)
-		printf("%d  ", newpic3[i]);
+		printf("%d  ", pic[i]);
 	printf("\n****************************\n");
 
 	delete tmp1;
 }
 
-
-void init()
+void applyFilterToTexture(Filter filter, void(*function)(GLubyte*), int width, int height)
 {
-	FILE *f, *f1;
-	//int picSize, picSize1;
-	int rd;
-	GLubyte header[54], colorTable[1024];
-	glEnable(GL_TEXTURE_2D);
-	glOrtho(-1.0, 1.0, -1.0, 1.0, 2.0, -2.0);
-	glClearColor(0, 0, 0, 0);
+	if (function != NULL)	function(newPics[filter]);
 
-	f1 = fopen("lena256.bmp", "rb");
-
-	/*************************/
-	//image header reading
-	fread(header, 54, 1, f1);
-	if (header[0] != 'B' || header[1] != 'M')		exit(1);  //not a BMP file
-
-	width1 = *(int*)(header + 18);
-	height1 = *(int*)(header + 22);
-	printf("\nlena %d %d %d \n", width1, height1, width1*height1);
-
-	/**********************************/
-
-	pic1 = new GLubyte[width1*height1];
-	newpic = new GLubyte[width1*height1];
-	newpic2 = new GLubyte[width1*height1];
-	newpic3 = new GLubyte[width1*height1];
-	tmp2 = new GLubyte[height1*width1 * 4];
-
-	rd = fread(colorTable, 1, 1024, f1); //read color table
-	rd = fread(pic1, 1, width1*height1, f1); //read image
-
-	fclose(f1);
-
-
-	//************ first image **************************
-
-	glGenTextures(1, &texture[0]);  //generate place for new texture
-	glBindTexture(GL_TEXTURE_2D, texture[0]); // initialize first texure 
+	glGenTextures(1, &texture[filter]);  //generate place for new texture
+	glBindTexture(GL_TEXTURE_2D, texture[filter]); // initialize first texure 
 
 	//texture properties
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
@@ -238,58 +232,53 @@ void init()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 	//build texture
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1, height1, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, pic1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, newPics[filter]);
 
-
-	//************ second image **************************	
-
-	halftone(tmp2);
-
-	glGenTextures(1, &texture[1]);
-	glBindTexture(GL_TEXTURE_2D, texture[1]);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-	writeArrayToFile('5', tmp2, width1*height1 * 4, TRUE);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1 * 2, height1 * 2, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, tmp2);
-
-
-	//************ third image **************************	
-	details();
-	glGenTextures(1, &texture[2]);
-	glBindTexture(GL_TEXTURE_2D, texture[2]);
-
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1, height1, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, newpic2);
-
-
-	//************ forth image **************************	
-
-	dithering();
-	glGenTextures(1, &texture[3]);
-	glBindTexture(GL_TEXTURE_2D, texture[3]);
-
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1, height1, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, newpic3);
 }
 
+void init()
+{
+	FILE* f1;
+	int rd;
+	GLubyte header[54], colorTable[1024];
+	glEnable(GL_TEXTURE_2D);
+	glOrtho(-1.0, 1.0, -1.0, 1.0, 2.0, -2.0);
+	glClearColor(0, 0, 0, 0);
 
+	fopen_s(&f1, "lena256.bmp", "rb");
+
+	/*************************/
+	//image header reading
+	fread(header, 54, 1, f1);
+	if (header[0] != 'B' || header[1] != 'M')		exit(1);  //not a BMP file
+
+	original_width = *(int*)(header + 18);
+	original_height = *(int*)(header + 22);
+	printf("\nlena %d %d %d \n", original_width, original_height, original_width*original_height);
+
+	/**********************************/
+
+	originalPic			= new GLubyte[original_width*original_height];
+	newPics[F_NONE] = originalPic;
+	newPics[F_SOBEL]	= new GLubyte[original_width*original_height];
+	newPics[F_HALFTONE] = new GLubyte[original_height*original_width * 4];
+	newPics[F_FLOYD]	= new GLubyte[original_width*original_height];
+
+	rd = fread(colorTable, 1, 1024, f1); //read color table
+	rd = fread(originalPic, 1, original_width*original_height, f1); //read image
+
+	fclose(f1);
+
+
+	applyFilterToTexture(F_NONE,		NULL,		original_width, original_height);
+	applyFilterToTexture(F_SOBEL,		sobel,		original_width, original_height);
+	applyFilterToTexture(F_HALFTONE,	halftone,	original_width*2, original_height*2);
+	applyFilterToTexture(F_FLOYD,		floyd,		original_width, original_height);
+
+	writeArrayToFile('4', newPics[F_SOBEL],		original_width*original_height,		TRUE);
+	writeArrayToFile('5', newPics[F_HALFTONE],	original_width*original_height*4,	TRUE);
+	writeArrayToFile('6', newPics[F_FLOYD],		original_width*original_height,		FALSE);
+}
 
 void paintQuadWithTexture(GLint x, GLint y, GLsizei width, GLsizei height, GLuint textureKey)
 {
@@ -319,17 +308,14 @@ void mydisplay(void){
 
 	glColor3f(1.0f, 1.0f, 1.0f);
 
-	paintQuadWithTexture(0,	  0,   256, 256, texture[1]);	//halftone
-	paintQuadWithTexture(256, 256, 256, 256, texture[2]);
-	paintQuadWithTexture(0,	  256, 256, 256, texture[0]);
-	paintQuadWithTexture(256, 0,   256, 256, texture[3]);
+	paintQuadWithTexture(0,	  256, 256, 256, texture[F_NONE]);
+	paintQuadWithTexture(256, 256, 256, 256, texture[F_SOBEL]);
+	paintQuadWithTexture(0,	  0,   256, 256, texture[F_HALFTONE]);
+	paintQuadWithTexture(256, 0,   256, 256, texture[F_FLOYD]);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glFlush();
 }
-
-
-
 
 int main(int  argc, char** argv)
 {
@@ -343,9 +329,6 @@ int main(int  argc, char** argv)
 	glutMainLoop();
 
 
-	delete pic1;	//original
-	delete newpic;	
-	delete tmp2;	//halftone. X4 in size
-	delete newpic2;
-	delete newpic3;
+	//delete originalPic;	//original
+	delete[] newPics;
 }
