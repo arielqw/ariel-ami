8c7f04cb87f8376eab10b33ab7003462e723aa2f
arielbaruch <arielbaruch@ubuntu>
2015-04-01 07:39:21 -0700

    fff

---
+++ xv6/picachu.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[]){
+	printf(1, "no exit call here.. \n");
+
+	return 0;
+}
---
+++ xv6/picachu.d
@@ -0,0 +1,1 @@
+picachu.o: picachu.c types.h stat.h user.h
